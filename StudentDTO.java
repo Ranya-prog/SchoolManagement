2c7a1612e9bc94d76e5868b251a977c6447019e3
Ranya-prog <raniahassouni200@gmail.com>
2024-09-11 17:35:12 +0100

    Create StudentDTO.java

---
+++ StudentDTO.java
@@ -0,0 +1,30 @@
+{
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+}
